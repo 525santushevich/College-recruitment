bdc247a060518577843e6a23e7cb24ae284618b3
525santushevich <525santushevich@frhsd.com>
2024-09-11 13:48:37 -0400

    some of nav

---
+++ script.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("mySidenav").classList.add("open");
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").classList.remove("open");
+}
+
